83ecd929d39f3fb8558b2a1957d104ff687ce6d8
JNyaga <joelnyagamuriithi@gmail.com>
2025-02-13 23:26:28 +0300

    Implement the signup flow

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,26 @@
+# app/controllers/registrations_controller.rb
+class RegistrationsController < ApplicationController
+  allow_unauthenticated_access only: [ :new, :create ]
+  before_action :resume_session, only: [ :new, :create ]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for(@user)
+      redirect_to root_path, notice: "Welcome! You have signed up successfully."
+    else
+      flash.now[:alert] = @user.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :password, :password_confirmation)
+  end
+end
