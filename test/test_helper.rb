9efdb4d5194c98089ec1e95e27883a703021abee
JNyaga <joelnyagamuriithi@gmail.com>
2025-02-15 10:47:42 +0300

    Add SimpleCov for test coverage reporting; update .gitignore and Gemfile

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,20 @@
+# Start SimpleCov before anything else
+require "simplecov"
+
+SimpleCov.start "rails" do
+  add_filter "/bin/"
+  add_filter "/db/"
+  add_filter "/config/"
+  add_filter "/test/"  # Exclude test files themselves
+end
+
+# Ensure SimpleCov merges coverage results correctly when using parallel tests
+if ENV["PARALLEL_WORKERS"]
+  SimpleCov.at_exit do
+    SimpleCov.result.format!
+  end
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
